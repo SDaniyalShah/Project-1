a207c0b046a3228a41d78d90b63f4304770cac60
SDaniyalShah <danyalshah59@gmail.com>
2024-11-08 14:42:20 +0500

    Files Added

--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -1,10 +1,15 @@
-export default function Jobs () {
+'use client';
+import { useEffect } from "react";
 
-    throw new Error('Job page not yet implimented')
-    return (
-        <div className= "bg-gray-900 bg-cover px-2 h-screen pt-6 font-semibold text-white text-lg justify-center" >
-            <h1>Error 500 Internal Server Error</h1>
-        </div>
-    );
+export default function Jobs () {
+    useEffect( function () {
+
+    throw new Error('Job page not yet implimented');
+    }, []);
+    // return (
+    //     <div className= "bg-gray-900 bg-cover px-2 h-screen pt-6 font-semibold text-white text-lg justify-center" >
+    //         <h1>Error 500 Internal Server Error</h1>
+    //     </div>
+    // );
     
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import Image from "next/image";
+//import Image from "next/image";
 import { redirect } from "next/navigation";
-export default function home() {
-  redirect ("/home")
+ export default function home() {
+ redirect ("/home")
   return (
-    <div> <h1>Home</h1>
-    </div>)}
-//     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <div> <h1>Home</h1>
+     </div>)}
+// //     {(<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
 //       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
 //         <Image
 //           className="dark:invert"
